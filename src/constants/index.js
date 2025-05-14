e404e27295b800e06f3ad87192615526994da353
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2025-05-13 17:53:53 -0700

    feat: Update file structure and add password reset email template

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 import path from 'node:path';
 
-export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+export const TEMPLATES_FOLDER = path.join(process.cwd(), 'src', 'templates');
 
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
-export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+export const TEMP_FOLDER = path.join(process.cwd(), 'temp');
+export const UPLOAD_FOLDER = path.join(process.cwd(), 'uploads');
 
 // JWT token expiration times
 export const ACCESS_TOKEN_EXPIRES_IN = '15m';
